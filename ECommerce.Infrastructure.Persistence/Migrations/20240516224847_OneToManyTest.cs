c4128fee79220b51100a919891136112c360d802
alperTunca <atunca51@hotmail.com>
2024-05-17 01:54:37 +0300

    - User-Order, one-to-many relation test.

---
+++ ECommerce.Infrastructure.Persistence/Migrations/20240516224847_OneToManyTest.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ECommerce.Infrastructure.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class OneToManyTest : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "OrderId",
+                table: "Orders");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Orders_UserId",
+                table: "Orders",
+                column: "UserId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Orders_Users_UserId",
+                table: "Orders",
+                column: "UserId",
+                principalTable: "Users",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Orders_Users_UserId",
+                table: "Orders");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Orders_UserId",
+                table: "Orders");
+
+            migrationBuilder.AddColumn<int>(
+                name: "OrderId",
+                table: "Orders",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+        }
+    }
+}
